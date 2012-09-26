206a144544e003b521e3527ad63b76ca5168ee3a
sedley <sedley@mail.usf.edu>
2012-09-26 14:22:36 -0400

    Simulation of hardware using random card id's and location codes

---
+++ README.md
@@ -0,0 +1,6 @@
+SCANNER
+=======
+
+This repo holds the software that runs on the custom tracking and permission
+managment system that will be adopted by the BSA for large events.  Each user
+is given a LoCo magnetic stripe card which holds their assigned id. Scans (punches in and out) are stored in a local MySQL database.  
---
+++ scanner.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import MySQLdb as mdb
+import sys
+import random as random
+con = None
+
+try:
+	con = mdb.connect('localhost','scanner_test','promethus','scanner'); #Not final sql details
+	cur = con.cursor()
+	cardid=random.randint(1000000000000000000,9000000000000000000) #Create a random card id
+	locationid=random.randint(1,99) #Create a random location id
+	cur.execute("use scanner") #Make sure we're in the right table
+	idcheck="SELECT * FROM scanner WHERE card_id = {0}".format(cardid) #Prepare check statment			
+	cur.execute(idcheck)
+	checkresult=cur.fetchone()
+	print(checkresult)
+	if checkresult is None:
+		sqlstring="INSERT INTO scanner (card_id, punch_in_or_out,location_code) VALUES({0}, 'IN', {1});".format(cardid,locationid) #Punch the user in
+	elif checkresult is not None:
+		sqlstring="INSERT INTO scanner (card_id, punch_in_or_out,location_code) VALUES({0}, 'OUT', {1});".format(cardid,locationid) #Punch the user out
+	
+	#print(checkresult)	DEBUG STATMENTS
+	#print(sqlstring)	DEBUG STATMENTS
+	cur.execute(sqlstring) #Execute the actual SQL transaction
+	con.commit() #Commit the change to the db
+
+except mdb.Error, e:
+	con.rollback() #Something went really wrong
+	print "error %d: %s" % (e.args[0],e.args[1])
+	sys.exit(1)
+
+finally:
+	if con:
+		con.close()
+
+	
