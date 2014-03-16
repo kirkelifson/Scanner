a241e557d13cc816ba870713bb77ccf55550d70d
kirkelifson <kirk@parodybit.net>
2014-03-16 22:39:11 +0000

    updated

--- isbn.py
+++ isbn.py
@@ -1,13 +1,11 @@
 import sys
 import random
-f = open('./isbn-list', 'w')
-runs=input("How many numbers? ")
-i=0
-k=0
-num=0
-isbn=['0']*13
+f = open('isbn-list', 'w')
+runs = input("How many numbers? ")
+i = k = num = 0
+isbn = ['0']*13
 for k in range(runs):
     for i in range(13):
-        f.write(str(random.randint(0,9)));
+        f.write(str(random.randint(0, 9)));
     f.write("\n")
 f.close()
